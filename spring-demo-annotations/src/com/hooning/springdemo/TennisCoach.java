72252390e5676b9b555782abeba0ee71f22af8ce
Hoon Cho <hch@autinity.de>
2021-06-04 12:46:03 +0200

    Setter and Field Injection examples

--- spring-demo-annotations/src/com/hooning/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/hooning/springdemo/TennisCoach.java
@@ -7,13 +7,35 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired // Field level injection (Java Reflection technology is used)
     private FortuneService fortuneService;
 
-    // from Spring 4.3 @Autowired annotation in constructor is no longer necessary (if there is more than one you need to specify in one constructor)
-    @Autowired
-    public TennisCoach(FortuneService theFortuneService) {
-        this.fortuneService = theFortuneService;
+    // define a default constructor
+    public TennisCoach() {
+        System.out.println(">> TennisCoach: inside default constructor");
     }
+
+    // from Spring 4.3 @Autowired annotation in constructor is no longer necessary (if there is more than one you need to specify in one constructor)
+
+    /* Using constructor for dependency injection */
+//    @Autowired // can be omitted only from constructor
+//    public TennisCoach(FortuneService theFortuneService) {
+//        this.fortuneService = theFortuneService;
+//    }
+
+    /* Using setter method for dependency injection */
+//    @Autowired // if it is not a constructor you need @Autowired annotation
+//    public void setFortuneService(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside default setFortuneService() method");
+//        this.fortuneService = fortuneService;
+//    }
+
+    /* Using any method name for dependency injection */
+//    @Autowired // if it is not a constructor you need @Autowired annotation
+//    public void doSomeCrazyStuff(FortuneService fortuneService) {
+//        System.out.println(">> TennisCoach: inside default doSomeCrazyStuff() method");
+//        this.fortuneService = fortuneService;
+//    }
 
     // you need @Autowired Annotation if you have additional constructor
     // public TennisCoach() {}
